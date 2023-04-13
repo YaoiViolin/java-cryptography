cef1115027416abb8b22263479b9e098af49b139
19223229 <MPDyakovskaya@sberbank.ru>
2023-04-13 19:48:08 +0300

    Homework for 13.04.2023

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,34 +3,47 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.security.SecureRandom;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Random;
 
 public class Application {
 
+    public static final String BASIC = "basic";
+    public static final String SECURE = "secure";
+    public static final String EXIT = "exit";
+
     public static void main(String[] args) throws IOException {
-        Map<Boolean, String> forecastMap = Map.of(
+        Map<Boolean, String> forecasts = Map.of(
                 true, "У вас сегодня будет удача в делах!",
                 false, "Сегодня хороший день для саморазвития!"
         );
+        String randomType = "";
         String name = "";
-        Random random = new Random(System.currentTimeMillis());
 
-        while (!name.equals("exit")) {
+        while (!randomType.equals(EXIT)) {
+
+            System.out.println("Введите имя и нажмите Enter");
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
             name = reader.readLine();
-            String forecast = reader.readLine();
+
+            System.out.println("Введите тип прогноза и нажмите Enter илиexit, чтобы выйти");
+            randomType = reader.readLine().toLowerCase(Locale.ROOT);
+
             System.out.print(name + "! ");
-            switch (forecast) {
-                case "Basic":
-                    System.out.println(forecastMap.get(random.nextBoolean()));
+            switch (randomType) {
+                case BASIC:
+                    Random random = new Random(System.currentTimeMillis());
+                    System.out.println(forecasts.get(random.nextBoolean()));
                     break;
-                case "Secure":
+                case SECURE:
                     SecureRandom secureRandom = new SecureRandom(name.getBytes(StandardCharsets.UTF_8));
-                    System.out.println(forecastMap.get(secureRandom.nextBoolean()));
+                    System.out.println(forecasts.get(secureRandom.nextBoolean()));
+                    break;
+                case EXIT:
                     break;
                 default:
-                    System.out.println("Error");
+                    System.out.println("Wrong type");
             }
         }
     }
